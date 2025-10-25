63774f9ea21f3e6ac19081dbaea0303b52156bdb
Rémi Guillette <remiguillette@gmail.com>
2025-10-25 17:37:19 -0400

    Build React dashboard for Neutralino app

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,99 +1,149 @@
-// This is just a sample app. You can structure your Neutralinojs app code as you wish.
-// This example app is written with vanilla JavaScript and HTML.
-// Feel free to use any frontend framework you like :)
-// See more details: https://neutralino.js.org/docs/how-to/use-a-frontend-library
+(function () {
+  if (typeof window === 'undefined') {
+    return;
+  }
 
-/*
-    Function to display information about the Neutralino app.
-    This function updates the content of the 'info' element in the HTML
-    with details regarding the running Neutralino application, including
-    its ID, port, operating system, and version information.
-*/
-function showInfo() {
-    document.getElementById('info').innerHTML = `
-        ${NL_APPID} is running on port ${NL_PORT} inside ${NL_OS}
-        <br/><br/>
-        <span>server: v${NL_VERSION} . client: v${NL_CVERSION}</span>
-        `;
-}
+  function resolveAppInfo() {
+    return {
+      id: typeof NL_APPID !== 'undefined' ? NL_APPID : 'unknown-app',
+      port: typeof NL_PORT !== 'undefined' ? NL_PORT : 0,
+      os: typeof NL_OS !== 'undefined' ? NL_OS : 'Unknown OS',
+      version: typeof NL_VERSION !== 'undefined' ? NL_VERSION : '0.0.0',
+      clientVersion:
+        typeof NL_CVERSION !== 'undefined' ? NL_CVERSION : '0.0.0',
+    };
+  }
 
-/*
-    Function to open the official Neutralino documentation in the default web browser.
-*/
-function openDocs() {
-    Neutralino.os.open("https://neutralino.js.org/docs");
-}
+  function createFallbackBridge() {
+    return {
+      getAppInfo: resolveAppInfo,
+      openDocs: function () {
+        window.open('https://neutralino.js.org/docs', '_blank');
+      },
+      openTutorial: function () {
+        window.open('https://www.youtube.com/c/CodeZri', '_blank');
+      },
+      onTrayEvent: function () {
+        return function () {};
+      },
+      checkForUpdates: function () {
+        return Promise.resolve(resolveAppInfo().version);
+      },
+      sendLog: function () {},
+    };
+  }
 
-/*
-    Function to open a tutorial video on Neutralino's official YouTube channel in the default web browser.
-*/
-function openTutorial() {
-    Neutralino.os.open("https://www.youtube.com/c/CodeZri");
-}
+  if (typeof Neutralino === 'undefined') {
+    window.dashboardBridge = createFallbackBridge();
+    return;
+  }
 
-/*
-    Function to set up a system tray menu with options specific to the window mode.
-    This function checks if the application is running in window mode, and if so,
-    it defines the tray menu items and sets up the tray accordingly.
-*/
-function setTray() {
-    // Tray menu is only available in window mode
-    if(NL_MODE != "window") {
-        console.log("INFO: Tray menu is only available in the window mode.");
-        return;
+  var trayListeners = new Set();
+
+  function setTray() {
+    if (typeof NL_MODE !== 'undefined' && NL_MODE !== 'window') {
+      console.log('INFO: Tray menu is only available in the window mode.');
+      return;
     }
 
-    // Define tray menu items
-    let tray = {
-        icon: "/resources/icons/trayIcon.png",
-        menuItems: [
-            {id: "VERSION", text: "Get version"},
-            {id: "SEP", text: "-"},
-            {id: "QUIT", text: "Quit"}
-        ]
+    var tray = {
+      icon: '/resources/icons/trayIcon.png',
+      menuItems: [
+        { id: 'VERSION', text: 'Get version' },
+        { id: 'SEP', text: '-' },
+        { id: 'QUIT', text: 'Quit' },
+      ],
     };
 
-    // Set the tray menu
     Neutralino.os.setTray(tray);
-}
+  }
 
-/*
-    Function to handle click events on the tray menu items.
-    This function performs different actions based on the clicked item's ID,
-    such as displaying version information or exiting the application.
-*/
-function onTrayMenuItemClicked(event) {
-    switch(event.detail.id) {
-        case "VERSION":
-            // Display version information
-            Neutralino.os.showMessageBox("Version information",
-                `Neutralinojs server: v${NL_VERSION} | Neutralinojs client: v${NL_CVERSION}`);
-            break;
-        case "QUIT":
-            // Exit the application
-            Neutralino.app.exit();
-            break;
+  function notifyTrayListeners(id) {
+    var eventSnapshot = { id: id, timestamp: Date.now() };
+    trayListeners.forEach(function (listener) {
+      try {
+        listener(eventSnapshot);
+      } catch (error) {
+        console.error('Tray listener failed', error);
+      }
+    });
+  }
+
+  function onTrayMenuItemClicked(event) {
+    var detail = event && event.detail ? event.detail : { id: 'UNKNOWN' };
+
+    switch (detail.id) {
+      case 'VERSION':
+        Neutralino.os.showMessageBox(
+          'Version information',
+          'Neutralinojs server: v' +
+            NL_VERSION +
+            ' | Neutralinojs client: v' +
+            NL_CVERSION
+        );
+        break;
+      case 'QUIT':
+        Neutralino.app.exit();
+        break;
     }
-}
 
-/*
-    Function to handle the window close event by gracefully exiting the Neutralino application.
-*/
-function onWindowClose() {
+    notifyTrayListeners(detail.id);
+  }
+
+  function onWindowClose() {
     Neutralino.app.exit();
-}
+  }
 
-// Initialize Neutralino
-Neutralino.init();
+  Neutralino.init();
+  Neutralino.events.on('trayMenuItemClicked', onTrayMenuItemClicked);
+  Neutralino.events.on('windowClose', onWindowClose);
 
-// Register event listeners
-Neutralino.events.on("trayMenuItemClicked", onTrayMenuItemClicked);
-Neutralino.events.on("windowClose", onWindowClose);
-
-// Conditional initialization: Set up system tray if not running on macOS
-if(NL_OS != "Darwin") { // TODO: Fix https://github.com/neutralinojs/neutralinojs/issues/615
+  if (typeof NL_OS !== 'undefined' && NL_OS !== 'Darwin') {
     setTray();
-}
+  }
 
-// Display app information
-showInfo();
+  window.dashboardBridge = {
+    getAppInfo: resolveAppInfo,
+    openDocs: function () {
+      Neutralino.os.open('https://neutralino.js.org/docs');
+    },
+    openTutorial: function () {
+      Neutralino.os.open('https://www.youtube.com/c/CodeZri');
+    },
+    onTrayEvent: function (listener) {
+      if (typeof listener !== 'function') {
+        return function () {};
+      }
+      trayListeners.add(listener);
+      return function () {
+        trayListeners.delete(listener);
+      };
+    },
+    checkForUpdates: function () {
+      return Neutralino.app
+        .getConfig()
+        .then(function (config) {
+          if (config && config.version) {
+            return config.version;
+          }
+          return resolveAppInfo().version;
+        })
+        .catch(function () {
+          return resolveAppInfo().version;
+        });
+    },
+    sendLog: function (message) {
+      if (
+        Neutralino.debug &&
+        typeof Neutralino.debug.log === 'function' &&
+        typeof message === 'string'
+      ) {
+        try {
+          Neutralino.debug.log(message);
+        } catch (error) {
+          console.error('Unable to write to Neutralino log', error);
+        }
+      }
+    },
+  };
+})();
